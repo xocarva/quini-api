a0608ed2bcd15b268ec9de2c5e5ff479c53c255e
xocarva <correo@xoancarneiro.com>
2023-09-17 11:18:45 +0200

    transform teams ids from string to ObjectId in leagueDay schema

--- src/api/v1/schemas/leagueDay.schema.ts
+++ src/api/v1/schemas/leagueDay.schema.ts
@@ -1,24 +1,36 @@
 import z from 'zod';
 import { teamWithIdSchema } from './team.schema';
+import { ObjectId } from 'mongodb';
 
 export const leagueDaySchema = z.object({
   season: z.string(),
   leagueDayNumber: z.coerce.number(),
-  rowsData: z.array(z.object({
-    position: z.number().min(1).max(16),
-    homeTeamId: z.string(),
-    awayTeamId: z.string(),
-    result: z.enum(['0', '1', '2', 'm', 'x']).optional(),
-  })).length(16),
+  rowsData: z.array(
+    z.object({
+      position: z.number().min(1).max(16),
+      homeTeamId: z.string().refine((value) => ObjectId.isValid(value), {
+        message: 'homeTeamId not valid',
+      }).transform((value) => new ObjectId(value)),
+      awayTeamId: z.string().refine((value) => ObjectId.isValid(value), {
+        message: 'awayTeamId not valid',
+      }).transform((value) => new ObjectId(value)),
+      result: z.enum(['0', '1', '2', 'm', 'x']).optional(),
+    }),
+  ).length(16),
 });
 
 export const partialLeagueDaySchema = leagueDaySchema.extend({
-  rowsData: z.array(z.object({
-    position: z.number().min(1).max(16),
-    homeTeamId: z.string(),
-    awayTeamId: z.string(),
-    result: z.enum(['0', '1', '2', 'm', 'x']).optional(),
-  })),
+  rowsData: z.array(
+    z.object({
+      position: z.number().min(1).max(16),
+      homeTeamId: z.string().refine((value) => ObjectId.isValid(value), {
+        message: 'homeTeamId not valid',
+      }).transform((value) => new ObjectId(value)),
+      awayTeamId: z.string().refine((value) => ObjectId.isValid(value), {
+        message: 'awayTeamId not valid',
+      }).transform((value) => new ObjectId(value)),
+      result: z.enum(['0', '1', '2', 'm', 'x']).optional(),
+    })),
 }).partial();
 
 export const leagueDayWithIdSchema = leagueDaySchema.extend({ id: z.string() });
